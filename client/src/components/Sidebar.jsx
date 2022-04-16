09533c52e49ef012da299f52402e2dd1260cc688
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-04-17 01:25:54 +0500

    sidebar done

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,171 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  ArrowLeft,
+  Check,
+  ChevronDown,
+  Circle,
+  Menu,
+  MessageSquare,
+  Search,
+} from "react-feather";
 
 export default function Sidebar() {
   return (
     <div className="container__sidebar">
-      <div className="container__sidebar__header">header</div>
-      <div className="container__sidebar__search">search</div>
-      <div className="container__sidebar__list">list</div>
+      <SidebarHeader />
+      <SidebarSearch />
+      <SidebarList />
+    </div>
+  );
+}
+
+function SidebarHeader({}) {
+  return (
+    <div className="container__sidebar__header">
+      <button className="container__sidebar__header__avatar">
+        <img
+          src="https://pps.whatsapp.net/v/t61.24694-24/245227693_702756124181740_640416741522230386_n.jpg?ccb=11-4&oh=01_AVwrE1GHnnWSEEA4LeQUx9wzXzCwQO_wzod87vJ9OTgQmQ&oe=6269D1D6"
+          alt="avatar"
+          className="container__sidebar__header__avatar__img"
+        />
+      </button>
+      <div className="container__sidebar__header__actions">
+        <button
+          className="container__sidebar__header__actions__entry"
+          title="status"
+        >
+          <Circle size={16} color="currentColor" />
+        </button>
+        <button
+          className="container__sidebar__header__actions__entry"
+          title="new chat"
+        >
+          <MessageSquare size={16} color="currentColor" />
+        </button>
+        <button
+          className="container__sidebar__header__actions__entry"
+          title="menu"
+        >
+          <Menu size={16} color="currentColor" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function SidebarSearch({}) {
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <div className="container__sidebar__search">
+      <form className="container__sidebar__search__form">
+        <button className="container__sidebar__search__form__btn">
+          {isFocused ? (
+            <ArrowLeft size={16} color="currentColor" />
+          ) : (
+            <Search size={16} color="currentColor" />
+          )}
+        </button>
+        <input
+          type="search"
+          onFocus={() => {
+            setIsFocused(true);
+          }}
+          onBlur={() => {
+            setIsFocused(false);
+          }}
+          placeholder="Search"
+          className="container__sidebar__search__form__input"
+        />
+      </form>
+    </div>
+  );
+}
+
+function SidebarList({}) {
+  return (
+    <div className="container__sidebar__list">
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+      <SidebarListEntry />
+    </div>
+  );
+}
+
+function SidebarListEntry({}) {
+  function getText(text) {
+    if (text.length > 30) {
+      return text.substring(0, 30) + "...";
+    } else {
+      return text;
+    }
+  }
+  return (
+    <div className="container__sidebar__list__entry">
+      <input
+        type="radio"
+        className="container__sidebar__list__entry__input"
+        name="container__sidebar__list__entry__input"
+      />
+      <div className="container__sidebar__list__entry__wrapper">
+        <img
+          src="https://pps.whatsapp.net/v/t61.24694-24/245227693_702756124181740_640416741522230386_n.jpg?ccb=11-4&oh=01_AVwrE1GHnnWSEEA4LeQUx9wzXzCwQO_wzod87vJ9OTgQmQ&oe=6269D1D6"
+          alt="avatar"
+          className="container__sidebar__list__entry__img"
+        />
+        <div className="container__sidebar__list__entry__content">
+          <div className="container__sidebar__list__entry__content__top">
+            <div className="container__sidebar__list__entry__content__top__heading">
+              Group name
+            </div>
+            <div className="container__sidebar__list__entry__content__top__sub__heading">
+              12:42 AM
+            </div>
+          </div>
+          <div className="container__sidebar__list__entry__content__bottom">
+            <div className="container__sidebar__list__entry__content__bottom__heading">
+              <Check size={12} color="currentColor" />
+              {getText(
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet quam dicta, officia perferendis magni delectus distinctio possimus corrupti maiores architecto."
+              )}
+            </div>
+            <button
+              onClick={() => {
+                console.log("hey");
+              }}
+              className="container__sidebar__list__entry__content__bottom__btn"
+            >
+              <ChevronDown size={16} color="currentColor" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
